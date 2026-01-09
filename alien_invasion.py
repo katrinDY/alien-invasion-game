81af8a257e812b8fd1c6fae0eff2557d42e7d484
katrinDY <katrin.yordanova@crust.tech>
2026-01-09 18:54:06 +0200

    Move events and screen setting into a sep function in the AlienInvasion class

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,18 +21,23 @@ class AlienInvasion:
   def run_game(self):
     """Start the main loop for the game"""
     while True:
-      # Watch for keyboard and mouse events.
-      for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-          sys.exit()
-      
-      # Redraw the screen during each pass through the loop
-      self.screen.fill(self.settings.bg_color)
-      # Add alien ship
-      self.alien_ship.blitme()
-      # Make the most recently drawn screen visible
-      pygame.display.flip()
-    
+      self.check_events()
+      self.update_screen()
+
+  def check_events(self):
+    # Watch for keyboard and mouse events.
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        sys.exit()
+
+  def update_screen(self):
+    # Redraw the screen during each pass through the loop
+    self.screen.fill(self.settings.bg_color)
+    # Add alien ship
+    self.alien_ship.blitme()
+    # Make the most recently drawn screen visible
+    pygame.display.flip()
+
 if __name__ == '__main__':
   # Make a game instance and run the game.
   ai = AlienInvasion()
