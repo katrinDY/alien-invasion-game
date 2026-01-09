5fc8b2a5f3985b867d554637dff7d1372a87051f
katrinDY <katrin.yordanova@crust.tech>
2026-01-09 18:11:53 +0200

    Add AlienShip class

---
+++ alien-ship.py
@@ -0,0 +1,19 @@
+import pygame
+
+class AlienShip:
+  """A class to manage the alien ship"""
+  
+  def __init__(self, ai_game):
+    self.screen = ai_game.screen
+    self.screen_rect = ai_game.screen.get_rect()
+  
+    # Load the alien ship and get its rect
+    self.image = pygame.image.load('images/alien_ship.bmp')
+    self.rect = self.image.get_rect()
+  
+    # Start each new ship at the bottom center of the screen
+    self.rect.midbottom = self.screen_rect.midbottom
+  
+  def blitme(self):
+    """Draw the ship at its current location"""
+    self.screen.blit(self.image, self.rect)
