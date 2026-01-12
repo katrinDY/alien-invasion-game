89a6b1ced6225b8db8d4b56c5c1dcb7080e4b019
katrinDY <katrin.yordanova@crust.tech>
2026-01-12 17:40:24 +0200

    Make ship faster

--- alien_ship.py
+++ alien_ship.py
@@ -5,6 +5,7 @@ class AlienShip:
   
   def __init__(self, ai_game):
     self.screen = ai_game.screen
+    self.settings = ai_game.settings
     self.screen_rect = ai_game.screen.get_rect()
   
     # Load the alien ship and get its rect
@@ -13,7 +14,10 @@ class AlienShip:
   
     # Start each new ship at the bottom center of the screen
     self.rect.midbottom = self.screen_rect.midbottom
-  
+
+    # Store a decimal value for the ship's horizontal position
+    self.x = float(self.rect.x)
+
     # Movement flag
     self.moving_right = False
     self.moving_left = False
@@ -21,10 +25,13 @@ class AlienShip:
   def update(self):
     """Update the ship's position based on movement flags"""
     if self.moving_right:
-      self.rect.x += 1
+      self.x += self.settings.ship_speed
     elif self.moving_left:
-      self.rect.x -= 1
-  
+      self.x -= self.settings.ship_speed
+
+    # Update rectangular objects from self.x
+    self.rect.x = self.x
+
   def blitme(self):
     """Draw the ship at its current location"""
     self.screen.blit(self.image, self.rect)
