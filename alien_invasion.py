a65a4b68ef452655fc8a2f474ce23253d545a279
katrinDY <katrin.yordanova@crust.tech>
2026-01-27 21:55:17 +0200

    Keep alien fleet on the screen after reaching an right/left edge

--- alien_invasion.py
+++ alien_invasion.py
@@ -123,7 +123,9 @@ class AlienInvasion:
         self.bullets.remove(bullet)
 
   def _update_aliens(self):
-    """Update the positions of all aliens in the fleet"""
+    """Check if the fleet is at an edge,
+    then update the positions of all aliens in the fleet"""
+    self._check_fleet_edges()
     self.aliens.update()
 
   def update_screen(self):
@@ -141,6 +143,19 @@ class AlienInvasion:
 
     # Make the most recently drawn screen visible
     pygame.display.flip()
+  
+  def _check_fleet_edges(self):
+    """Respond appropriately if any alien hits the edge"""
+    for alien in self.aliens.sprites():
+      if alien.check_edges():
+        self._change_fleet_direction()
+        break
+
+  def _change_fleet_direction(self):
+    """Drop the entire fleet and change the fleet's direction"""
+    for alien in self.aliens.sprites():
+      alien.rect.y += self.settings.fleet_drop_speed
+    self.settings.fleet_direction *= -1
 
 if __name__ == '__main__':
   # Make a game instance and run the game.
